980357aa8e97e1e77c7addc99c6d3116d6d481ab
KKaged <dhigaredaleon707@gmail.com>
2024-01-17 22:35:12 +0900

    wiped off some cobwebs,rounds work

--- script.js
+++ script.js
@@ -3,36 +3,44 @@ const moves = ["Rock", "Paper", "Scissors"];
 const rock = document.querySelector(".rock");
 const scissors = document.querySelector(".scissors");
 const paper = document.querySelector(".paper");
+const playerScore = document.querySelector(".playerScore");
+const computersScore = document.querySelector(".computerScore");
+const finalResult = document.querySelector(".finalResults");
+const result = document.querySelector(".result");
 let playerSelection;
+let computerSelection;
+let winnerScore = 0;
+let computerScore = 0;
+let winner;
+
+//What the computer picks
+const newComputerChoice = () => {
+  computerSelection = moves[Math.floor(Math.random() * moves.length)];
+  return computerSelection;
+};
 
 rock.addEventListener("click", () => {
   console.log(`The Computer Picks ${computerSelection}`);
   playerSelection = "Rock";
   console.log(`You Picked Rock!`);
-
-  console.log(playRound(playerSelection, computerSelection));
+  playRound(playerSelection);
 });
 scissors.addEventListener("click", () => {
   console.log(`The Computer Picks ${computerSelection}`);
   playerSelection = "Scissors";
   console.log("You picked Scissors!");
-  console.log(playRound(playerSelection, computerSelection));
+  playRound(playerSelection);
 });
 paper.addEventListener("click", () => {
   console.log(`The Computer Picks ${computerSelection}`);
   playerSelection = "Paper";
   console.log("You Picked Paper!");
-  console.log(playRound(playerSelection, computerSelection));
+  playRound(playerSelection);
 });
 
-//What the computer picks
-function getComputerChoice() {
-  return moves[Math.floor(Math.random() * moves.length)];
-}
 //one round
-function playRound(playerSelection, computerSelection) {
-  let winner;
-
+function playRound(playerSelection) {
+  newComputerChoice();
   if (playerSelection === computerSelection) {
     winner = "It's a tie!";
   } else if (
@@ -40,11 +48,30 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "Paper" && computerSelection === "Rock") ||
     (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
+    winnerScore++;
     winner = `You Win! The Computer Picked ${computerSelection}`;
+    playerScore.innerText = `Your Score: ${winnerScore}`;
+    console.log(`Your Score: ` + winnerScore);
   } else {
     winner = `Computer Wins with ${computerSelection}!`;
+    computerScore++;
+    computersScore.innerText = `Computer Score: ${computerScore}`;
+    console.log(`Comp ` + computerScore);
   }
-
   document.querySelector(".result").innerText = winner;
+  if (winnerScore === 5) {
+    finalResult.innerText = "You Win!";
+    winnerScore = 0;
+    computerScore = 0;
+    playerScore.innerText = `Your Score: ${winnerScore}`;
+    computersScore.innerText = `Computer Score: ${computerScore}`;
+  } else {
+    if (computerScore === 5) {
+      finalResult.innerText = "Computer Wins!";
+      winnerScore = 0;
+      computerScore = 0;
+      playerScore.innerText = `Your Score: ${winnerScore}`;
+      computersScore.innerText = `Computer Score: ${computerScore}`;
+    }
+  }
 }
-const computerSelection = getComputerChoice();
